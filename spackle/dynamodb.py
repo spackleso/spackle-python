26dfed4e4f48331f19530551ef2579968250c3e7
Hunter Clarke <hunter@hunterclarke.me>
2022-12-17 21:57:17 -0700

    0.0.6

--- spackle/dynamodb.py
+++ spackle/dynamodb.py
@@ -3,9 +3,10 @@ import requests
 from datetime import datetime, timezone
 
 _aws_access_key_id = None
+_aws_region = None
 _aws_secret_access_key = None
-_aws_session_token = None
 _aws_session_expiration = None
+_aws_session_token = None
 
 table_name = None
 identity_id = None
@@ -17,6 +18,7 @@ def get_client():
         or _aws_secret_access_key is None
         or _aws_session_token is None
         or _aws_session_expiration is None
+        or _aws_region is None
         or _aws_session_expiration < datetime.now(timezone.utc)
     ):
         _fetch_credentials()
@@ -26,6 +28,7 @@ def get_client():
         aws_access_key_id=_aws_access_key_id,
         aws_secret_access_key=_aws_secret_access_key,
         aws_session_token=_aws_session_token,
+        region_name=_aws_region,
     )
 
 
@@ -35,6 +38,7 @@ def _fetch_credentials():
     global table_name
     global identity_id
     global _aws_access_key_id
+    global _aws_region
     global _aws_secret_access_key
     global _aws_session_token
     global _aws_session_expiration
@@ -54,6 +58,7 @@ def _fetch_credentials():
     identity_id = session["identity_id"]
     table_name = session["table_name"]
     _aws_access_key_id = credentials["Credentials"]["AccessKeyId"]
+    _aws_region = session["aws_region"]
     _aws_secret_access_key = credentials["Credentials"]["SecretAccessKey"]
     _aws_session_token = credentials["Credentials"]["SessionToken"]
     _aws_session_expiration = credentials["Credentials"]["Expiration"]
