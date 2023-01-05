bc57c8c3dae1a8be9467fa5b4f6e0908b77efa88
Hunter Clarke <hunter@hunterclarke.me>
2023-01-05 08:52:30 -0700

    updates tests to new dynamodb store

--- tests/test_customer.py
+++ tests/test_customer.py
@@ -1,35 +1,44 @@
 import spackle
 
+from unittest import mock
+
 spackle.api_key = "abc123"
 
 
 class TestCustomer:
-    def test_retrieve(self, requests_mock):
-        requests_mock.post(
-            "https://api.spackle.so/auth/session",
-            json={},
-        )
-        spackle.Customer.retrieve("cus_123")
-        assert requests_mock.call_count == 1
-        req = requests_mock.last_request
-        assert req.headers["Authorization"] == "Bearer abc123"
+    def test_retrieve(self):
+        mock_client = mock.Mock()
+        mock_client.client.get_item.return_value = {
+            "Item": {
+                "State": {
+                    "S": '{"subscriptions": [], "features": [{"key": "foo", "value_flag": true}]}'
+                }
+            }
+        }
+
+        with mock.patch("spackle.dynamodb.get_client", return_value=mock_client):
+            customer = spackle.Customer.retrieve("cus_123")
+
+        assert customer.enabled("foo") is True
 
     def test_enabled(self):
         customer = spackle.Customer(
+            "cus_123",
             data={
                 "subscriptions": [],
                 "features": [{"key": "foo", "value_flag": True}],
-            }
+            },
         )
         assert customer.enabled("foo") is True
         assert customer.enabled("bar") is False
 
     def test_limit(self):
         customer = spackle.Customer(
+            "cus_123",
             data={
                 "subscriptions": [],
                 "features": [{"key": "foo", "value_limit": 100}],
-            }
+            },
         )
         assert customer.limit("foo") == 100
         assert customer.limit("bar") == 0
