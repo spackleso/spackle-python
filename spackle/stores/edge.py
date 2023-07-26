7a7dc1b32c8ff6e22dd097a9c989f1f31c2a8a78
Hunter Clarke <hunter@hunterclarke.me>
2023-07-26 17:50:47 -0600

    uses requests and makes it thread safe

--- spackle/stores/edge.py
+++ spackle/stores/edge.py
@@ -1,49 +1,56 @@
 import json
-import urllib3
+import requests
+import threading
 
 from spackle import log
 from spackle.exceptions import SpackleException
 
 
 class EdgeStore:
+    _thread_local = None
+
     def __init__(self):
-        self.http = urllib3.PoolManager()
+        self._thread_local = threading.local()
 
     def get_customer_data(self, customer_id):
         from spackle import api_key, edge_base, schema_version
 
-        response = self.http.request(
-            "GET",
+        if getattr(self._thread_local, "session", None) is None:
+            self._thread_local.session = requests.Session()
+
+        response = self._thread_local.session.get(
             f"{edge_base}/customers/{customer_id}/state",
             headers={
                 "Authorization": f"Bearer {api_key}",
-                "X-Spackle-Schema-Version": schema_version,
+                "X-Spackle-Schema-Version": str(schema_version),
             },
         )
 
-        if response.status != 200:
+        if response.status_code != 200:
             return self._fetch_state_from_api(customer_id)
 
-        body = response.data.decode("utf-8")
-        log.log_debug("Retrieved customer data for %s: %s" % (customer_id, body))
-        return json.loads(body)
+        data = response.json()
+        log.log_debug("Retrieved customer data for %s: %s" % (customer_id, data))
+        return data
 
     def _fetch_state_from_api(self, customer_id):
-        from spackle import api_key, api_base
+        from spackle import api_key, api_base, schema_version
 
         log.log_warn("Customer %s not found in store, using API" % customer_id)
-        response = self.http.request(
-            "GET",
+        response = requests.get(
             f"{api_base}/customers/{customer_id}/state",
-            headers={"Authorization": f"Bearer {api_key}"},
+            headers={
+                "Authorization": f"Bearer {api_key}",
+                "X-Spackle-Schema-Version": str(schema_version),
+            },
         )
 
-        if response.status != 200:
+        if response.status_code != 200:
             raise SpackleException(
                 "Customer %s not found; status code: %s; response: %s"
-                % (customer_id, response.status, response.data.decode("utf-8"))
+                % (customer_id, response.status_code, response.text)
             )
 
-        body = response.data.decode("utf-8")
-        log.log_debug("Retrieved customer data for %s: %s" % (customer_id, body))
-        return json.loads(body)
+        data = response.json()
+        log.log_debug("Retrieved customer data for %s: %s" % (customer_id, data))
+        return data
--- tests/test_edge.py
+++ tests/test_edge.py
@@ -10,30 +10,38 @@ spackle.api_key = "abc123"
 class TestEdgeStore:
     def test_retrieve(self):
         mock_response = mock.Mock()
-        mock_response.data = b'{"subscriptions": [], "features": [{"key": "foo", "value_flag": true, "type": 0}]}'
-        mock_response.status = 200
+        mock_response.json.return_value = {
+            "subscriptions": [],
+            "features": [{"key": "foo", "value_flag": True, "type": 0}],
+        }
+        mock_response.status_code = 200
 
         mock_client = mock.Mock()
-        mock_client.request.return_value = mock_response
-        with mock.patch("urllib3.PoolManager", return_value=mock_client):
+        mock_client.get.return_value = mock_response
+        with mock.patch("requests.Session", return_value=mock_client):
             spackle.set_store(EdgeStore())
+            customer = spackle.Customer.retrieve("cus_123")
 
-        customer = spackle.Customer.retrieve("cus_123")
         assert customer.enabled("foo") is True
 
     def test_retrieve_fallback_to_api(self):
         mock_error_response = mock.Mock()
-        mock_error_response.status = 404
-        mock_error_response.data = b""
+        mock_error_response.status_code = 404
+        mock_error_response.text.return_value = ""
+
+        mock_error_client = mock.Mock()
+        mock_error_client.get.return_value = mock_error_response
 
         mock_response = mock.Mock()
-        mock_response.data = b'{"subscriptions": [], "features": [{"key": "foo", "value_flag": true, "type": 0}]}'
-        mock_response.status = 200
+        mock_response.json.return_value = {
+            "subscriptions": [],
+            "features": [{"key": "foo", "value_flag": True, "type": 0}],
+        }
+        mock_response.status_code = 200
 
-        mock_client = mock.Mock()
-        mock_client.request.side_effect = [mock_error_response, mock_response]
-        with mock.patch("urllib3.PoolManager", return_value=mock_client):
-            spackle.set_store(EdgeStore())
+        with mock.patch("requests.Session", return_value=mock_error_client):
+            with mock.patch("requests.get", return_value=mock_response):
+                spackle.set_store(EdgeStore())
+                customer = spackle.Customer.retrieve("cus_123")
 
-        customer = spackle.Customer.retrieve("cus_123")
         assert customer.enabled("foo") is True
